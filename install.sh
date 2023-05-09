750de0f76f795aa80bcc7becfdd585f78482b205
Rajarshi <rajarshi99@gmail.com>
2023-05-09 11:19:12 +0530

    sh script make and rm

---
+++ install.sh
@@ -0,0 +1,11 @@
+HERE=/home/rajarshi/vimux
+
+# Neovim folder
+ln -s $HERE/.config/nvim .config/nvim
+
+# Tmux conf and folder
+ln -s $HERE/.tmux.conf .tmux.conf
+ln -s $HERE/.tmux .tmux
+
+# Terminal folders
+ln -s $HERE/.term .term
---
+++ uninstall.sh
@@ -0,0 +1,11 @@
+HERE=/home/rajarshi/vimux
+
+# Neovim folder
+rm -rf .config/nvim
+
+# Tmux conf and folder
+rm -rf .tmux.conf
+rm -rf .tmux
+
+# Terminal folders
+rm -rf .term
