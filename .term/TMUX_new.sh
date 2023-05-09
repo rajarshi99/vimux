46ec4b6d34658bdc53492a9e8f5d8921fc7c203e
Rajarshi <rajarshi99@gmail.com>
2023-05-09 12:37:34 +0530

    Added fzf for tmux nav

--- .term/TMUX_new.sh
+++ .term/TMUX_new.sh
@@ -2,7 +2,7 @@ KEY=$1
 HERE=/home/rajarshi/.term
 cd $HOME
 find . -not -path '*/.*' -type d \
-	| rofi -dmenu -i \
+	| fzf --bind=tab:up,btab:down \
 	| awk -v key=$KEY -v home="/home/rajarshi" -f $HERE/cd.awk \
 	>> $HERE/place.txt
 awk -v key=$KEY -v tmux=1 -f $HERE/cd.awk $HERE/place.txt | xargs tmux new-window -c
